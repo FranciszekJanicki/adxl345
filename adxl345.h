3f811a0fd3c006c946fca4a4421e3c420e24a198
Franciszek Janicki <franciszek.janicki1@gmail.com>
2025-07-30 13:21:14 +0200

    changes

--- adxl345.h
+++ adxl345.h
@@ -5,152 +5,152 @@
 #include "adxl345_registers.h"
 
 typedef struct {
-    adxl345_config_t config;
-    adxl345_interface_t interface;
+  adxl345_config_t config;
+  adxl345_interface_t interface;
 } adxl345_t;
 
-adxl345_err_t adxl345_initialize(adxl345_t* adxl345,
-                                 adxl345_config_t const* config,
-                                 adxl345_interface_t const* interface);
-adxl345_err_t adxl345_deinitialize(adxl345_t* adxl345);
+adxl345_err_t adxl345_initialize(adxl345_t *adxl345,
+                                 adxl345_config_t const *config,
+                                 adxl345_interface_t const *interface);
+adxl345_err_t adxl345_deinitialize(adxl345_t *adxl345);
 
-adxl345_err_t adxl345_get_acc_data_x_scaled(adxl345_t const* adxl345,
-                                            float32_t* scaled);
-adxl345_err_t adxl345_get_acc_data_y_scaled(adxl345_t const* adxl345,
-                                            float32_t* scaled);
-adxl345_err_t adxl345_get_acc_data_z_scaled(adxl345_t const* adxl345,
-                                            float32_t* scaled);
-adxl345_err_t adxl345_get_acc_data_scaled(adxl345_t const* adxl345,
-                                          vec3_float32_t* scaled);
+adxl345_err_t adxl345_get_acc_data_x_scaled(adxl345_t const *adxl345,
+                                            float32_t *scaled);
+adxl345_err_t adxl345_get_acc_data_y_scaled(adxl345_t const *adxl345,
+                                            float32_t *scaled);
+adxl345_err_t adxl345_get_acc_data_z_scaled(adxl345_t const *adxl345,
+                                            float32_t *scaled);
+adxl345_err_t adxl345_get_acc_data_scaled(adxl345_t const *adxl345,
+                                          vec3_float32_t *scaled);
 
-adxl345_err_t adxl345_get_acc_data_x_raw(adxl345_t const* adxl345,
-                                         int16_t* raw);
-adxl345_err_t adxl345_get_acc_data_y_raw(adxl345_t const* adxl345,
-                                         int16_t* raw);
-adxl345_err_t adxl345_get_acc_data_z_raw(adxl345_t const* adxl345,
-                                         int16_t* raw);
-adxl345_err_t adxl345_get_acc_data_raw(adxl345_t const* adxl345,
-                                       vec3_int16_t* raw);
+adxl345_err_t adxl345_get_acc_data_x_raw(adxl345_t const *adxl345,
+                                         int16_t *raw);
+adxl345_err_t adxl345_get_acc_data_y_raw(adxl345_t const *adxl345,
+                                         int16_t *raw);
+adxl345_err_t adxl345_get_acc_data_z_raw(adxl345_t const *adxl345,
+                                         int16_t *raw);
+adxl345_err_t adxl345_get_acc_data_raw(adxl345_t const *adxl345,
+                                       vec3_int16_t *raw);
 
-adxl345_err_t adxl345_get_devid_reg(adxl345_t const* adxl345,
-                                    adxl345_devid_reg_t* reg);
+adxl345_err_t adxl345_get_devid_reg(adxl345_t const *adxl345,
+                                    adxl345_devid_reg_t *reg);
 
-adxl345_err_t adxl345_get_thresh_tap_reg(adxl345_t const* adxl345,
-                                         adxl345_thresh_tap_reg_t* reg);
-adxl345_err_t adxl345_set_thresh_tap_reg(adxl345_t const* adxl345,
-                                         adxl345_thresh_tap_reg_t const* reg);
+adxl345_err_t adxl345_get_thresh_tap_reg(adxl345_t const *adxl345,
+                                         adxl345_thresh_tap_reg_t *reg);
+adxl345_err_t adxl345_set_thresh_tap_reg(adxl345_t const *adxl345,
+                                         adxl345_thresh_tap_reg_t const *reg);
 
-adxl345_err_t adxl345_get_ofsx_reg(adxl345_t const* adxl345,
-                                   adxl345_ofsx_reg_t* reg);
-adxl345_err_t adxl345_set_ofsx_reg(adxl345_t const* adxl345,
-                                   adxl345_ofsx_reg_t const* reg);
+adxl345_err_t adxl345_get_ofsx_reg(adxl345_t const *adxl345,
+                                   adxl345_ofsx_reg_t *reg);
+adxl345_err_t adxl345_set_ofsx_reg(adxl345_t const *adxl345,
+                                   adxl345_ofsx_reg_t const *reg);
 
-adxl345_err_t adxl345_get_ofsy_reg(adxl345_t const* adxl345,
-                                   adxl345_ofsy_reg_t* reg);
-adxl345_err_t adxl345_set_ofsy_reg(adxl345_t const* adxl345,
-                                   adxl345_ofsy_reg_t const* reg);
+adxl345_err_t adxl345_get_ofsy_reg(adxl345_t const *adxl345,
+                                   adxl345_ofsy_reg_t *reg);
+adxl345_err_t adxl345_set_ofsy_reg(adxl345_t const *adxl345,
+                                   adxl345_ofsy_reg_t const *reg);
 
-adxl345_err_t adxl345_get_ofsz_reg(adxl345_t const* adxl345,
-                                   adxl345_ofsz_reg_t* reg);
-adxl345_err_t adxl345_set_ofsz_reg(adxl345_t const* adxl345,
-                                   adxl345_ofsz_reg_t const* reg);
+adxl345_err_t adxl345_get_ofsz_reg(adxl345_t const *adxl345,
+                                   adxl345_ofsz_reg_t *reg);
+adxl345_err_t adxl345_set_ofsz_reg(adxl345_t const *adxl345,
+                                   adxl345_ofsz_reg_t const *reg);
 
-adxl345_err_t adxl345_get_dur_reg(adxl345_t const* adxl345,
-                                  adxl345_dur_reg_t* reg);
-adxl345_err_t adxl345_set_dur_reg(adxl345_t const* adxl345,
-                                  adxl345_dur_reg_t const* reg);
+adxl345_err_t adxl345_get_dur_reg(adxl345_t const *adxl345,
+                                  adxl345_dur_reg_t *reg);
+adxl345_err_t adxl345_set_dur_reg(adxl345_t const *adxl345,
+                                  adxl345_dur_reg_t const *reg);
 
-adxl345_err_t adxl345_get_latent_reg(adxl345_t const* adxl345,
-                                     adxl345_latent_reg_t* reg);
-adxl345_err_t adxl345_set_latent_reg(adxl345_t const* adxl345,
-                                     adxl345_latent_reg_t const* reg);
+adxl345_err_t adxl345_get_latent_reg(adxl345_t const *adxl345,
+                                     adxl345_latent_reg_t *reg);
+adxl345_err_t adxl345_set_latent_reg(adxl345_t const *adxl345,
+                                     adxl345_latent_reg_t const *reg);
 
-adxl345_err_t adxl345_get_window_reg(adxl345_t const* adxl345,
-                                     adxl345_window_reg_t* reg);
-adxl345_err_t adxl345_set_window_reg(adxl345_t const* adxl345,
-                                     adxl345_window_reg_t const* reg);
+adxl345_err_t adxl345_get_window_reg(adxl345_t const *adxl345,
+                                     adxl345_window_reg_t *reg);
+adxl345_err_t adxl345_set_window_reg(adxl345_t const *adxl345,
+                                     adxl345_window_reg_t const *reg);
 
-adxl345_err_t adxl345_get_thresh_act_reg(adxl345_t const* adxl345,
-                                         adxl345_thresh_act_reg_t* reg);
-adxl345_err_t adxl345_set_thresh_act_reg(adxl345_t const* adxl345,
-                                         adxl345_thresh_act_reg_t const* reg);
+adxl345_err_t adxl345_get_thresh_act_reg(adxl345_t const *adxl345,
+                                         adxl345_thresh_act_reg_t *reg);
+adxl345_err_t adxl345_set_thresh_act_reg(adxl345_t const *adxl345,
+                                         adxl345_thresh_act_reg_t const *reg);
 
-adxl345_err_t adxl345_get_thresh_inact_reg(adxl345_t const* adxl345,
-                                           adxl345_thresh_inact_reg_t* reg);
-adxl345_err_t adxl345_set_thresh_inact_reg(
-    adxl345_t const* adxl345,
-    adxl345_thresh_inact_reg_t const* reg);
+adxl345_err_t adxl345_get_thresh_inact_reg(adxl345_t const *adxl345,
+                                           adxl345_thresh_inact_reg_t *reg);
+adxl345_err_t
+adxl345_set_thresh_inact_reg(adxl345_t const *adxl345,
+                             adxl345_thresh_inact_reg_t const *reg);
 
-adxl345_err_t adxl345_get_time_inact_reg(adxl345_t const* adxl345,
-                                         adxl345_time_inact_reg_t* reg);
-adxl345_err_t adxl345_set_time_inact_reg(adxl345_t const* adxl345,
-                                         adxl345_time_inact_reg_t const* reg);
+adxl345_err_t adxl345_get_time_inact_reg(adxl345_t const *adxl345,
+                                         adxl345_time_inact_reg_t *reg);
+adxl345_err_t adxl345_set_time_inact_reg(adxl345_t const *adxl345,
+                                         adxl345_time_inact_reg_t const *reg);
 
-adxl345_err_t adxl345_get_act_inact_ctl_reg(adxl345_t const* adxl345,
-                                            adxl345_act_inact_ctl_reg_t* reg);
-adxl345_err_t adxl345_set_act_inact_ctl_reg(
-    adxl345_t const* adxl345,
-    adxl345_act_inact_ctl_reg_t const* reg);
+adxl345_err_t adxl345_get_act_inact_ctl_reg(adxl345_t const *adxl345,
+                                            adxl345_act_inact_ctl_reg_t *reg);
+adxl345_err_t
+adxl345_set_act_inact_ctl_reg(adxl345_t const *adxl345,
+                              adxl345_act_inact_ctl_reg_t const *reg);
 
-adxl345_err_t adxl345_get_thresh_ff_reg(adxl345_t const* adxl345,
-                                        adxl345_thresh_ff_reg_t* reg);
-adxl345_err_t adxl345_set_thresh_ff_reg(adxl345_t const* adxl345,
-                                        adxl345_thresh_ff_reg_t const* reg);
+adxl345_err_t adxl345_get_thresh_ff_reg(adxl345_t const *adxl345,
+                                        adxl345_thresh_ff_reg_t *reg);
+adxl345_err_t adxl345_set_thresh_ff_reg(adxl345_t const *adxl345,
+                                        adxl345_thresh_ff_reg_t const *reg);
 
-adxl345_err_t adxl345_get_time_ff_reg(adxl345_t const* adxl345,
-                                      adxl345_time_ff_reg_t* reg);
-adxl345_err_t adxl345_set_time_ff_reg(adxl345_t const* adxl345,
-                                      adxl345_time_ff_reg_t const* reg);
+adxl345_err_t adxl345_get_time_ff_reg(adxl345_t const *adxl345,
+                                      adxl345_time_ff_reg_t *reg);
+adxl345_err_t adxl345_set_time_ff_reg(adxl345_t const *adxl345,
+                                      adxl345_time_ff_reg_t const *reg);
 
-adxl345_err_t adxl345_get_tap_axes_reg(adxl345_t const* adxl345,
-                                       adxl345_tap_axes_reg_t* reg);
-adxl345_err_t adxl345_set_tap_axes_reg(adxl345_t const* adxl345,
-                                       adxl345_tap_axes_reg_t const* reg);
+adxl345_err_t adxl345_get_tap_axes_reg(adxl345_t const *adxl345,
+                                       adxl345_tap_axes_reg_t *reg);
+adxl345_err_t adxl345_set_tap_axes_reg(adxl345_t const *adxl345,
+                                       adxl345_tap_axes_reg_t const *reg);
 
-adxl345_err_t adxl345_get_act_tap_status_reg(adxl345_t const* adxl345,
-                                             adxl345_act_tap_status_reg_t* reg);
+adxl345_err_t adxl345_get_act_tap_status_reg(adxl345_t const *adxl345,
+                                             adxl345_act_tap_status_reg_t *reg);
 
-adxl345_err_t adxl345_get_bw_rate_reg(adxl345_t const* adxl345,
-                                      adxl345_bw_rate_reg_t* reg);
-adxl345_err_t adxl345_set_bw_rate_reg(adxl345_t const* adxl345,
-                                      adxl345_bw_rate_reg_t const* reg);
+adxl345_err_t adxl345_get_bw_rate_reg(adxl345_t const *adxl345,
+                                      adxl345_bw_rate_reg_t *reg);
+adxl345_err_t adxl345_set_bw_rate_reg(adxl345_t const *adxl345,
+                                      adxl345_bw_rate_reg_t const *reg);
 
-adxl345_err_t adxl345_get_power_ctl_reg(adxl345_t const* adxl345,
-                                        adxl345_power_ctl_reg_t* reg);
-adxl345_err_t adxl345_set_power_ctl_reg(adxl345_t const* adxl345,
-                                        adxl345_power_ctl_reg_t const* reg);
+adxl345_err_t adxl345_get_power_ctl_reg(adxl345_t const *adxl345,
+                                        adxl345_power_ctl_reg_t *reg);
+adxl345_err_t adxl345_set_power_ctl_reg(adxl345_t const *adxl345,
+                                        adxl345_power_ctl_reg_t const *reg);
 
-adxl345_err_t adxl345_get_int_enable_reg(adxl345_t const* adxl345,
-                                         adxl345_int_enable_reg_t* reg);
-adxl345_err_t adxl345_set_int_enable_reg(adxl345_t const* adxl345,
-                                         adxl345_int_enable_reg_t const* reg);
+adxl345_err_t adxl345_get_int_enable_reg(adxl345_t const *adxl345,
+                                         adxl345_int_enable_reg_t *reg);
+adxl345_err_t adxl345_set_int_enable_reg(adxl345_t const *adxl345,
+                                         adxl345_int_enable_reg_t const *reg);
 
-adxl345_err_t adxl345_get_int_map_reg(adxl345_t const* adxl345,
-                                      adxl345_int_map_reg_t* reg);
-adxl345_err_t adxl345_set_int_map_reg(adxl345_t const* adxl345,
-                                      adxl345_int_map_reg_t const* reg);
+adxl345_err_t adxl345_get_int_map_reg(adxl345_t const *adxl345,
+                                      adxl345_int_map_reg_t *reg);
+adxl345_err_t adxl345_set_int_map_reg(adxl345_t const *adxl345,
+                                      adxl345_int_map_reg_t const *reg);
 
-adxl345_err_t adxl345_get_int_source_reg(adxl345_t const* adxl345,
-                                         adxl345_int_source_reg_t* reg);
+adxl345_err_t adxl345_get_int_source_reg(adxl345_t const *adxl345,
+                                         adxl345_int_source_reg_t *reg);
 
-adxl345_err_t adxl345_set_data_format_reg(adxl345_t const* adxl345,
-                                          adxl345_data_format_reg_t const* reg);
+adxl345_err_t adxl345_set_data_format_reg(adxl345_t const *adxl345,
+                                          adxl345_data_format_reg_t const *reg);
 
-adxl345_err_t adxl345_get_data_x_reg(adxl345_t const* adxl345,
-                                     adxl345_data_x_reg_t* reg);
-adxl345_err_t adxl345_get_data_y_reg(adxl345_t const* adxl345,
-                                     adxl345_data_y_reg_t* reg);
-adxl345_err_t adxl345_get_data_z_reg(adxl345_t const* adxl345,
-                                     adxl345_data_z_reg_t* reg);
-adxl345_err_t adxl345_get_data_reg(adxl345_t const* adxl345,
-                                   adxl345_data_reg_t* reg);
+adxl345_err_t adxl345_get_data_x_reg(adxl345_t const *adxl345,
+                                     adxl345_data_x_reg_t *reg);
+adxl345_err_t adxl345_get_data_y_reg(adxl345_t const *adxl345,
+                                     adxl345_data_y_reg_t *reg);
+adxl345_err_t adxl345_get_data_z_reg(adxl345_t const *adxl345,
+                                     adxl345_data_z_reg_t *reg);
+adxl345_err_t adxl345_get_data_reg(adxl345_t const *adxl345,
+                                   adxl345_data_reg_t *reg);
 
-adxl345_err_t adxl345_get_fifo_ctl_reg(adxl345_t const* adxl345,
-                                       adxl345_fifo_ctl_reg_t* reg);
-adxl345_err_t adxl345_set_fifo_ctl_reg(adxl345_t const* adxl345,
-                                       adxl345_fifo_ctl_reg_t const* reg);
+adxl345_err_t adxl345_get_fifo_ctl_reg(adxl345_t const *adxl345,
+                                       adxl345_fifo_ctl_reg_t *reg);
+adxl345_err_t adxl345_set_fifo_ctl_reg(adxl345_t const *adxl345,
+                                       adxl345_fifo_ctl_reg_t const *reg);
 
-adxl345_err_t adxl345_get_fifo_status_reg(adxl345_t const* adxl345,
-                                          adxl345_fifo_status_reg_t* reg);
+adxl345_err_t adxl345_get_fifo_status_reg(adxl345_t const *adxl345,
+                                          adxl345_fifo_status_reg_t *reg);
 
 #endif // ADXL345_ADXL345_H
--- adxl345_registers.h
+++ adxl345_registers.h
@@ -4,167 +4,167 @@
 #include <stdint.h>
 
 typedef struct {
-    uint8_t devid : 8;
+  uint8_t devid : 8;
 } adxl345_devid_reg_t;
 
 typedef struct {
-    uint8_t thresh_tap : 8;
+  uint8_t thresh_tap : 8;
 } adxl345_thresh_tap_reg_t;
 
 typedef struct {
-    int8_t ofsx : 8;
+  int8_t ofsx : 8;
 } adxl345_ofsx_reg_t;
 
 typedef struct {
-    int8_t ofsy : 8;
+  int8_t ofsy : 8;
 } adxl345_ofsy_reg_t;
 
 typedef struct {
-    int8_t ofsz : 8;
+  int8_t ofsz : 8;
 } adxl345_ofsz_reg_t;
 
 typedef struct {
-    uint8_t dur : 8;
+  uint8_t dur : 8;
 } adxl345_dur_reg_t;
 
 typedef struct {
-    uint8_t latent : 8;
+  uint8_t latent : 8;
 } adxl345_latent_reg_t;
 
 typedef struct {
-    uint8_t window : 8;
+  uint8_t window : 8;
 } adxl345_window_reg_t;
 
 typedef struct {
-    uint8_t thresh_act : 8;
+  uint8_t thresh_act : 8;
 } adxl345_thresh_act_reg_t;
 
 typedef struct {
-    uint8_t thresh_inact : 8;
+  uint8_t thresh_inact : 8;
 } adxl345_thresh_inact_reg_t;
 
 typedef struct {
-    uint8_t time_inact : 8;
+  uint8_t time_inact : 8;
 } adxl345_time_inact_reg_t;
 
 typedef struct {
-    uint8_t act_ac_dc : 1;
-    uint8_t act_x_en : 1;
-    uint8_t act_y_en : 1;
-    uint8_t act_z_en : 1;
-    uint8_t inact_ac_dc : 1;
-    uint8_t inact_x_en : 1;
-    uint8_t inact_y_en : 1;
-    uint8_t inact_z_en : 1;
+  uint8_t act_ac_dc : 1;
+  uint8_t act_x_en : 1;
+  uint8_t act_y_en : 1;
+  uint8_t act_z_en : 1;
+  uint8_t inact_ac_dc : 1;
+  uint8_t inact_x_en : 1;
+  uint8_t inact_y_en : 1;
+  uint8_t inact_z_en : 1;
 } adxl345_act_inact_ctl_reg_t;
 
 typedef struct {
-    uint8_t thresh_ff : 8;
+  uint8_t thresh_ff : 8;
 } adxl345_thresh_ff_reg_t;
 
 typedef struct {
-    uint8_t time_ff : 8;
+  uint8_t time_ff : 8;
 } adxl345_time_ff_reg_t;
 
 typedef struct {
-    uint8_t suppress : 1;
-    uint8_t tap_x_en : 1;
-    uint8_t tap_y_en : 1;
-    uint8_t tap_z_en : 1;
+  uint8_t suppress : 1;
+  uint8_t tap_x_en : 1;
+  uint8_t tap_y_en : 1;
+  uint8_t tap_z_en : 1;
 } adxl345_tap_axes_reg_t;
 
 typedef struct {
-    uint8_t act_x_src : 1;
-    uint8_t act_y_src : 1;
-    uint8_t act_z_src : 1;
-    uint8_t asleep : 1;
-    uint8_t tap_x_src : 1;
-    uint8_t tap_y_src : 1;
-    uint8_t tap_z_src : 1;
+  uint8_t act_x_src : 1;
+  uint8_t act_y_src : 1;
+  uint8_t act_z_src : 1;
+  uint8_t asleep : 1;
+  uint8_t tap_x_src : 1;
+  uint8_t tap_y_src : 1;
+  uint8_t tap_z_src : 1;
 } adxl345_act_tap_status_reg_t;
 
 typedef struct {
-    uint8_t low_power : 1;
-    uint8_t rate : 4;
+  uint8_t low_power : 1;
+  uint8_t rate : 4;
 } adxl345_bw_rate_reg_t;
 
 typedef struct {
-    uint8_t link : 1;
-    uint8_t auto_sleep : 1;
-    uint8_t measure : 1;
-    uint8_t sleep : 1;
-    uint8_t wakeup : 2;
+  uint8_t link : 1;
+  uint8_t auto_sleep : 1;
+  uint8_t measure : 1;
+  uint8_t sleep : 1;
+  uint8_t wakeup : 2;
 } adxl345_power_ctl_reg_t;
 
 typedef struct {
-    uint8_t data_ready : 1;
-    uint8_t single_tap : 1;
-    uint8_t double_tap : 1;
-    uint8_t activity : 1;
-    uint8_t inactivity : 1;
-    uint8_t free_fall : 1;
-    uint8_t watermark : 1;
-    uint8_t overrun : 1;
+  uint8_t data_ready : 1;
+  uint8_t single_tap : 1;
+  uint8_t double_tap : 1;
+  uint8_t activity : 1;
+  uint8_t inactivity : 1;
+  uint8_t free_fall : 1;
+  uint8_t watermark : 1;
+  uint8_t overrun : 1;
 } adxl345_int_enable_reg_t;
 
 typedef struct {
-    uint8_t data_ready : 1;
-    uint8_t single_tap : 1;
-    uint8_t double_tap : 1;
-    uint8_t activity : 1;
-    uint8_t inactivity : 1;
-    uint8_t free_fall : 1;
-    uint8_t watermark : 1;
-    uint8_t overrun : 1;
+  uint8_t data_ready : 1;
+  uint8_t single_tap : 1;
+  uint8_t double_tap : 1;
+  uint8_t activity : 1;
+  uint8_t inactivity : 1;
+  uint8_t free_fall : 1;
+  uint8_t watermark : 1;
+  uint8_t overrun : 1;
 } adxl345_int_map_reg_t;
 
 typedef struct {
-    uint8_t data_ready : 1;
-    uint8_t single_tap : 1;
-    uint8_t double_tap : 1;
-    uint8_t activity : 1;
-    uint8_t inactivity : 1;
-    uint8_t free_fall : 1;
-    uint8_t watermark : 1;
-    uint8_t overrun : 1;
+  uint8_t data_ready : 1;
+  uint8_t single_tap : 1;
+  uint8_t double_tap : 1;
+  uint8_t activity : 1;
+  uint8_t inactivity : 1;
+  uint8_t free_fall : 1;
+  uint8_t watermark : 1;
+  uint8_t overrun : 1;
 } adxl345_int_source_reg_t;
 
 typedef struct {
-    uint8_t self_test : 1;
-    uint8_t spi : 1;
-    uint8_t int_invert : 1;
-    uint8_t full_res : 1;
-    uint8_t justify : 1;
-    uint8_t range : 2;
+  uint8_t self_test : 1;
+  uint8_t spi : 1;
+  uint8_t int_invert : 1;
+  uint8_t full_res : 1;
+  uint8_t justify : 1;
+  uint8_t range : 2;
 } adxl345_data_format_reg_t;
 
 typedef struct {
-    int16_t data_x : 16;
+  int16_t data_x : 16;
 } adxl345_data_x_reg_t;
 
 typedef struct {
-    int16_t data_y : 16;
+  int16_t data_y : 16;
 } adxl345_data_y_reg_t;
 
 typedef struct {
-    int16_t data_z : 16;
+  int16_t data_z : 16;
 } adxl345_data_z_reg_t;
 
 typedef struct {
-    int16_t data_x : 16;
-    int16_t data_y : 16;
-    int16_t data_z : 16;
+  int16_t data_x : 16;
+  int16_t data_y : 16;
+  int16_t data_z : 16;
 } adxl345_data_reg_t;
 
 typedef struct {
-    uint8_t fifo_mode : 2;
-    uint8_t trigger : 1;
-    uint8_t samples : 5;
+  uint8_t fifo_mode : 2;
+  uint8_t trigger : 1;
+  uint8_t samples : 5;
 } adxl345_fifo_ctl_reg_t;
 
 typedef struct {
-    uint8_t fifo_trig : 1;
-    uint8_t entries : 6;
+  uint8_t fifo_trig : 1;
+  uint8_t entries : 6;
 } adxl345_fifo_status_reg_t;
 
 #endif // ADXL345_ADXL345_REGISTERS_H
